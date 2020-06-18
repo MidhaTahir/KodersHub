2495defd23c6439ec941353a1d6e1fc3852b7004
Tooba Ali <toobaalined@gmail.com>
2020-06-18 14:01:01 +0500

    Add text editor with iframe

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import CodeArea from './components/code-area/code-area.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+ 
+const App = () => {
+	const [ inputText, setInputText ] = useState({
+		html: '',
+		css: '',
+		js: ''
+	});
+
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setInputText((prevValue) => {
+			return {
+				...prevValue,
+				[name]: value
+			};
+		});
+	};
+
+	return (
+		<div className="App">
+			<h3>Code Playground</h3>
+
+			{/* text areas */}
+			<CodeArea func={handleChange} lang="html" />
+			<CodeArea func={handleChange} lang="css" />
+			<CodeArea func={handleChange} lang="js" />
+
+			<div>
+				<iframe
+					id="iFrame"
+					srcDoc={
+						inputText.html +
+						'<style>' +
+						inputText.css +
+						'</style>' +
+						'<script>' +
+						inputText.js +
+						'</script>'
+					}
+				/>
+			</div>
+		</div>
+	);
+};
 
 export default App;
