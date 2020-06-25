52f9699495cf04148c5963ab2a4a332d0c7bcb95
Midha <midhatahirkhan2011@gmail.com>
2020-06-25 17:09:08 +0500

    edited css testing to read from file
    Co-authored-by: MidhaTahir <MidhaTahir@users.noreply.github.com>
    Co-authored-by: Muhammad Hasham <Muhammad-Hasham-Khalid@users.noreply.github.com>
    Co-authored-by: Tooba Ali <toobaali1@users.noreply.github.com>
    Co-authored-by: Fareena Ehtesham <FareenaEhtesham@users.noreply.github.com>

--- testing/testing-css/index.js
+++ testing/testing-css/index.js
@@ -5,23 +5,29 @@ const cssParse = require("./CSSJSON");
 var _ = require('lodash');
 
 // reading CSS files
-let defaultCss = `h1 {
-  color: black;
-  font-size: 24px;
-  background-color: #fff;
-}
-`,
-  userCss = `h1 {
-    color: black;
-    background-color: #fff;
-    font-size: 24px;
+let defaultCss = '',userCss = '';
+
+fs.readFile(path.join(__dirname,'stylesheet','default.txt'),'utf-8',(err,data)=>{
+  if(err){
+    console.log(err)
+    return
   }
-  `;
+  defaultCss = data
+  fs.readFile(path.join(__dirname,'stylesheet','user.txt'),'utf-8',(err,data)=>{
+    if(err){
+      console.log(err)
+      return
+    }
+    userCss = data
+    defaultCss = cssParse.toJSON(defaultCss)
+    userCss = cssParse.toJSON(userCss)
 
-defaultCss = cssParse.toJSON(defaultCss)
-userCss = cssParse.toJSON(userCss)
+    let flag =  _.isEqual(defaultCss, userCss)
 
-let flag =  _.isEqual(defaultCss, userCss)
+    if (flag) console.log("Test Passed!!!");
+    else console.log("Test Failed");
+  })
+  
+})
 
-if (flag) console.log("Test Passed!!!");
-else console.log("Test Failed");
+
