5fdf2b3068bdcad0072264c3bef508770c99e958
Midha <midhatahirkhan2011@gmail.com>
2020-07-04 22:05:59 +0500

    implement cors and function

--- client/src/pages/Homepage/homepage.component.jsx
+++ client/src/pages/Homepage/homepage.component.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import './homepage-page.styles.css';
-import { ReactComponent as Kid } from '../../images/kids_illustration.svg';
+import React from "react";
+import "./homepage-page.styles.css";
+import { ReactComponent as Kid } from "../../images/kids_illustration.svg";
 
-const HomePage =() =>{
+const HomePage = () => {
 
-    return(
-        <div>
-        <h1 className="head">KodersGang</h1>
-        <div className="textBig">
+  function fetchData() {
+    fetch("http://localhost:5000")
+      .then((data) => data.text())
+      .then(res => console.log(res))
+      .catch((err) => console.log(err));
+  }
+
+  return (
+    <div>
+      <h1 className='head'>KodersGang</h1>
+      <div className='textBig'>
         C&#123;&#125;deIt <br></br> & Enjoy.
-        </div>
-        {/* https://digital-flowers.github.io/react-animated-css.html */}
+      </div>
+      <Kid className='kid' />
+      
+      {/* Test client-server connection */}
+      <button onClick={fetchData}>Submit</button>
+    </div>
+  );
+};
 
-        <Kid className="kid"/>
-        {/* <ImageAvatars /> */}
-        {/* <Blob /> */}
-        {/* <Front className="front"/> */}
-        </div>
-    )
-}
-
-export default HomePage
+export default HomePage;
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express")
 const app = express()
+var cors = require('cors');
+
+app.use(cors());
 
 app.get('/',function(req,res){
     res.send('Hello World')
