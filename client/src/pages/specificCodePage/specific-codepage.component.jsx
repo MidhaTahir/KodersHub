5696a2736037a9b9c84b182d55bfc119014b9779
Tooba Ali <toobaalined@gmail.com>
2020-07-08 11:54:29 +0500

    Integrate HTML Testing

--- client/src/pages/specificCodePage/specific-codepage.component.jsx
+++ client/src/pages/specificCodePage/specific-codepage.component.jsx
@@ -4,6 +4,9 @@ import Iframe from '../../components/iframe/iframe.component';
 import Switch from '@material-ui/core/Switch';
 import './specific-codepage.styles.css';
 import SubmitButton from '../../components/submit-button/submit-button.component';
+import SubmitModal from '../../components/submitModal/submitModal.component';
+
+import axios from 'axios';
 
 const SpecificCodePage = (props) => {
 	const incomingLanguage = props.match.params.language;
@@ -20,15 +23,40 @@ const SpecificCodePage = (props) => {
 		setTheme(!theme);
 	};
 
+	const [ solution, setSolution ] = useState(false);
+	const [ testHasRun, setTestHasRun ] = useState(false);
+	// ------------------------------------------------------------------------------
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		// post to /test/lang to compute the solution
+		await axios.post(`/test/${incomingLanguage}`, { dataToTest: valueOfLang }).then((res) => {
+			// console.log(res);
+		});
+
+		// handling testing right after post request
+		const res = await fetch(`http://localhost:5000/test/${incomingLanguage}`);
+		const jsonRes = await res.json();
+		setSolution(jsonRes.sol);
+
+		setTestHasRun(true);
+	};
+
 	return (
 		<div className="code-area">
 			{/* text areas */}
-			<CodeArea func={handleChange} lang={incomingLanguage} inputText={valueOfLang} theme={theme} />
-			<p>
-				Light<Switch onClick={handleTheme} /> Dark
-			</p>
-			<Iframe lang={incomingLanguage} inputText={valueOfLang} />
-			<SubmitButton />
+			<form onSubmit={handleSubmit}>
+				<CodeArea func={handleChange} lang={incomingLanguage} inputText={valueOfLang} theme={theme} />
+				<p>
+					Light<Switch onClick={handleTheme} /> Dark
+				</p>
+				<Iframe lang={incomingLanguage} inputText={valueOfLang} />
+
+				<SubmitButton />
+				{/* ensuring that test is run before passing the solution */}
+				{testHasRun && <SubmitModal solution={solution} />}
+			</form>
 		</div>
 	);
 };
--- server.js
+++ server.js
@@ -1,13 +1,49 @@
-const express = require("express")
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
 var cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
-app.get('/',function(req,res){
-    res.send('Hello World')
-})
+// TESTING HTML CODE
+let comparedHTMLcode = true;
 
-app.listen(5000,()=>{
-    console.log("Server started at " + "....")
-})
+app.post('/test/html', (req, res) => {
+	let str = req.body.dataToTest; //str should come from body
+
+	let tag = 'h1';
+	let attribute = '';
+	let value = '';
+	let selfClosing = false;
+
+	if (selfClosing) {
+		var regex2 = new RegExp(`^<(${tag})\\s?((${attribute})\\s?=?"?${value}"?\\s?)\\/?>\\s?\n?$`, 'g');
+	} else {
+		var regex2 = new RegExp(
+			`<(${tag})\\s?((${attribute})\\s?=?"?${value}"?\\s?)\\/?>(\n?.*\n?<\\/\(${tag})>)`,
+			'g'
+		);
+	}
+
+	let result = str.match(regex2);
+
+	if (result == null) {
+		comparedHTMLcode = false;
+	} else {
+		comparedHTMLcode = true;
+	}
+
+	res.redirect('/test/html');
+});
+
+app.get('/test/html', (req, res) => {
+	// we will res.send true or false on the basis of which the popup will be shown
+	res.send({ sol: comparedHTMLcode });
+});
+
+app.listen(5000, () => {
+	console.log('Server started at ' + '....');
+});
