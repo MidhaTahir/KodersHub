719f35fe7a3c8c21cd123562a993ff710f51b3c3
Muhammad-Hasham-Khalid <m.hasham681@gmail.com>
2020-07-10 01:13:46 +0500

    Style dashboard UI (v1)
    Co-authored-by: Midha Tahir <MidhaTahir@users.noreply.github.com>
    Co-authored-by: Muhammad Hasham <Muhammad-Hasham-Khalid@users.noreply.github.com>

--- client/src/components/dashboard-cards/dashboard-cards.component.jsx
+++ client/src/components/dashboard-cards/dashboard-cards.component.jsx
@@ -1,19 +1,35 @@
-import React from 'react';
-import './dashboard-cards.styles.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./dashboard-cards.styles.css";
+import { Link } from "react-router-dom";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    flexDirection: "columnReverse",
+    "& > *": {
+      margin: theme.spacing(1),
+      width: theme.spacing(16),
+      height: theme.spacing(16),
+    },
+  },
+}));
 
 const DashboardCard = (props) => {
-	const linkStr = '/dashboard/' + props.lang;
-	return (
-		<div className="card-div">
-			<Link to={linkStr} style={{ textDecoration: 'none' }}>
-				<div className="dash-card">
-					<h1>{props.symbol}</h1>
-					<h2>{props.lang.toUpperCase()}</h2>
-				</div>
-			</Link>
-		</div>
-	);
+  const classes = useStyles();
+  const linkStr = "/dashboard/" + props.lang;
+  return (
+    <Paper className={classes.root} elevation={3} square={false}>
+      <Link to={linkStr} style={{ textDecoration: "none" }}>
+        <div>
+          <h1>{props.symbol}</h1>
+          <h2>{props.lang.toUpperCase()}</h2>
+        </div>
+      </Link>
+    </Paper>
+  );
 };
 
 export default DashboardCard;
--- client/src/pages/DashboardPage/dashboard-page.component.jsx
+++ client/src/pages/DashboardPage/dashboard-page.component.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import './dashboard-page.styles.css'
-import DashboardCard from '../../components/dashboard-cards/dashboard-cards.component';
+import React from "react";
+import "./dashboard-page.styles.css";
+import DashboardCard from "../../components/dashboard-cards/dashboard-cards.component";
 
 const DashboardPage = () => {
-	return (
-		<div>
-			<h1>This will be the dashboard from where you can select a language to work on!</h1>
-
-			<p>TODO: Show total score</p>
-			<p>TODO: Show 404 on other routes of dashboard/:language</p>
-			<DashboardCard lang="html" symbol="< >" />
-			<DashboardCard lang="css" symbol="{ }" />
-			<DashboardCard lang="javascript" symbol="{ ; }" />
-		</div>
-	);
+  return (
+    <div className="dashboard-page">
+      {/*
+      <h1>
+        This will be the dashboard from where you can select a language to work
+        on!
+      </h1>
+      <p>TODO: Show total score</p>
+      <p>TODO: Show 404 on other routes of dashboard/:language</p> 
+      */}
+      <DashboardCard lang="html" symbol="< >" />
+      <DashboardCard lang="css" symbol="{ }" />
+      <DashboardCard lang="javascript" symbol="{ ; }" />
+    </div>
+  );
 };
 
 export default DashboardPage;
