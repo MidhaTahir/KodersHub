2fb8ce8843b5579aefaeaa238c040d2e6a14974d
Muhammad-Hasham-Khalid <m.hasham681@gmail.com>
2020-07-10 15:45:09 +0500

    Correct link in cards & height in dashboard page

--- client/src/components/dashboard-cards/dashboard-cards.component.jsx
+++ client/src/components/dashboard-cards/dashboard-cards.component.jsx
@@ -8,23 +8,23 @@ const useStyles = makeStyles((theme) => ({
   root: ({ inactive }) => ({
     display: "flex",
     flexWrap: "wrap",
-    justifyContent : "space-evenly",
+    justifyContent: "space-evenly",
     textAlign: "center",
     borderRadius: theme.spacing(2),
     backgroundColor: "#F4F7FA",
     margin: theme.spacing(2),
-    boxShadow: '0px 10px 40px rgba(34, 35, 58, 0.2)',
-    transition: '0.3s',
+    boxShadow: "0px 10px 40px rgba(34, 35, 58, 0.2)",
+    transition: "0.3s",
     "& > *": {
       margin: theme.spacing(6),
       width: theme.spacing(20),
       height: theme.spacing(20),
-      marginTop : theme.spacing(14),
+      marginTop: theme.spacing(14),
       color: "#2FA08F",
     },
-    '&:hover': {
-      transform: 'translateY(2px)',
-      boxShadow: '0 4px 20px 0 rgba(0,0,0,0.12)',
+    "&:hover": {
+      transform: "translateY(2px)",
+      boxShadow: "0 4px 20px 0 rgba(0,0,0,0.12)",
     },
   }),
 }));
@@ -33,14 +33,14 @@ const DashboardCard = (props) => {
   const classes = useStyles();
   const linkStr = "/dashboard/" + props.lang;
   return (
-    <Paper className={classes.root} elevation={3} square={false}>
-      <Link to={linkStr} style={{ textDecoration: "none" }}>
+    <Link to={linkStr} style={{ textDecoration: "none" }}>
+      <Paper className={classes.root} elevation={3} square={false}>
         <div>
           <h1>{props.symbol}</h1>
           <h2>{props.lang.toUpperCase()}</h2>
         </div>
-      </Link>
-    </Paper>
+      </Paper>
+    </Link>
   );
 };
 
