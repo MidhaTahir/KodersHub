c5e74809a90ebe2407771dcacc43135fb78811b0
Muhammad-Hasham-Khalid <m.hasham681@gmail.com>
2020-07-14 19:33:15 +0500

    Correct React fragment

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,26 @@
-import React from 'react';
-import './App.css';
-import { Route, Switch } from 'react-router-dom';
-import Homepage from './pages/Homepage/homepage.component';
-import Header from './components/Header/header.component';
-import SignUp from './components/SignUp/signup.component';
-import SignIn from './components/SignIn/signin.component';
-import DashboardPage from './pages/DashboardPage/dashboard-page.component';
-import SpecificCodePage from './pages/specificCodePage/specific-codepage.component';
+import React from "react";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import Homepage from "./pages/Homepage/homepage.component";
+import Header from "./components/Header/header.component";
+import SignUp from "./components/SignUp/signup.component";
+import SignIn from "./components/SignIn/signin.component";
+import DashboardPage from "./pages/DashboardPage/dashboard-page.component";
+import SpecificCodePage from "./pages/specificCodePage/specific-codepage.component";
 
 const App = () => {
-	return (
-		<div>
-			<Header />
-
-			<Switch>
-				<Route exact path="/" component={Homepage} />
-				<Route exact path="/signUp/" component={SignUp}/>
-				<Route exact path="/signin/" component={SignIn} />
-				<Route exact path="/dashboard/" component={DashboardPage} />
-				<Route exact path="/dashboard/:language" component={SpecificCodePage} />
-			</Switch>
-		</div>
-	);
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/signUp/" component={SignUp} />
+        <Route exact path="/signin/" component={SignIn} />
+        <Route exact path="/dashboard/" component={DashboardPage} />
+        <Route exact path="/dashboard/:language" component={SpecificCodePage} />
+      </Switch>
+    </div>
+  );
 };
 
 export default App;
