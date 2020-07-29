2302f38404a3b0933d674a43bbbc3480d44dc003
Midha <midhatahirkhan2011@gmail.com>
2020-07-29 13:37:16 +0500

    Add Cube Rotation
    Co-authored-by: Midha Tahir <MidhaTahir@users.noreply.github.com>
    Co-authored-by: Hasham Khalid<Muhammad-Hasham-Khalid@users.noreply.github.com>

--- client/src/components/cubic/cubic.component.jsx
+++ client/src/components/cubic/cubic.component.jsx
@@ -1,32 +1,24 @@
 import React from "react";
-import Cube from "react-3d-cube";
 import "./cubic.styles.css";
-import back from "../../images/back.png";
-import html from "../../images/html.png";
-import css from "../../images/css.png";
-import js from "../../images/js.png";
-import sass from "../../images/sass.png";
+// import Cube from "react-3d-cube";
+// import back from "../../images/back.png";
+// import html from "../../images/html.png";
+// import css from "../../images/css.png";
+// import js from "../../images/js.png";
+// import sass from "../../images/sass.png";
 
 function Cubic() {
   return (
-    <div className='cubic'>
-        <div
-          style={{
-            width: 250,
-            height: 250,
-          }}
-        >
-          <Cube size={250} index='front'>
-            <img src={html} alt='front' />
-            <img src={css} alt='right' />
-            <img src={js} alt='back' />
-            <img src={sass} alt='left' />
-            <img src={back} alt='top' />
-            <img src={back} alt='bottom' />
-          </Cube>
-        </div>
-      
-    </div>
+    <div id="wrapD3Cube">
+	<div id="D3Cube">
+		<div id="side1"></div>
+		<div id="side2"></div>
+		<div id="side3"></div>
+		<div id="side4"></div>
+		<div id="side5"></div>
+		<div id="side6"></div>
+	</div>
+</div>
   );
 }
 
