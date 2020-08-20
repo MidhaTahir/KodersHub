0058f90c022da07ef20ec9cc09cdbd416e613b84
Muhammad-Hasham-Khalid <m.hasham681@gmail.com>
2020-08-20 12:26:19 +0500

    Add redirecting on logout

--- client/src/components/Header/header.component.jsx
+++ client/src/components/Header/header.component.jsx
@@ -1,20 +1,27 @@
 import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import './header.styles.css';
 import { Link } from 'react-router-dom';
 import ImageAvatars from '../avatar/avatar.component'
 import UserContext from "../../context/userContext"
 
-const Header = () => {
+const Header = props => {
 
 	const { user, LogoutUser } = useContext(UserContext);
 	console.log(user.username);
+	const history = useHistory();
+
+	function handleClick() {
+		LogoutUser();
+		history.push("/");
+	}
 
 	return (
 		<>
 			<div className="headerBody">
 				{
 					user.loggedIn
-					? <button onClick={LogoutUser} className="linkText">LOGOUT</button>
+					? <button onClick={handleClick} className="linkText">LOGOUT</button>
 					: <Link to="/signUp" className="linkText">SIGNUP</Link>
 				}
 				<Link to="/dashboard" className="linkText">DASHBOARD</Link>
